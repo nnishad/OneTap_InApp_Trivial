56e619ad7e5cd404b888e8071ed8f6e529beffbf
Nikhil Kumar Nishad <nikhilnishad279@gmail.com>
2019-09-22 14:43:14 +0530

    In-App fixed!

--- shared-module/src/main/java/com/example/billingmodule/billing/BillingConstants.java
+++ shared-module/src/main/java/com/example/billingmodule/billing/BillingConstants.java
@@ -25,12 +25,12 @@ import java.util.List;
  */
 public final class BillingConstants {
     // SKUs for our products: the premium upgrade (non-consumable) and gas (consumable)
-    public static final String SKU_PREMIUM = "test_product_1";
-    public static final String SKU_GAS = "test_product_1";
+    public static final String SKU_PREMIUM = "premium";
+    public static final String SKU_GAS = "gas";
 
     // SKU for our subscription (infinite gas)
-    public static final String SKU_GOLD_MONTHLY = "test_product_1";
-    public static final String SKU_GOLD_YEARLY = "test_product_1";
+    public static final String SKU_GOLD_MONTHLY = "gold_monthly";
+    public static final String SKU_GOLD_YEARLY = "gold_yearly";
 
     private static final String[] IN_APP_SKUS = {SKU_GAS, SKU_PREMIUM};
     private static final String[] SUBSCRIPTIONS_SKUS = {SKU_GOLD_MONTHLY, SKU_GOLD_YEARLY};
